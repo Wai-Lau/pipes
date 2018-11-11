32931887c82c94175bf0af5485152fbd49f7b668
Amanda <amawai@hotmail.com>
2018-11-11 00:36:59 -0500

    Add map

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,44 @@
+let GLOBAL_MAP = null;
+let USERS = {}
+
+// We take keys and markers so that we can populate the map with all active
+initMap = () => {
+    console.log('initializing map!!')
+    GLOBAL_MAP = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 20
+    });
+    var infoWindow = new google.maps.InfoWindow({map: GLOBAL_MAP});
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            placeAndBindMarker(pos, 'white', 'blue');
+            
+            GLOBAL_MAP.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, GLOBAL_MAP.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, GLOBAL_MAP.getCenter());
+    }
+}
+
+placeAndBindMarker = (latLng, borderColor, filledColor) => {
+    new google.maps.Marker({
+      position: latLng,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        fillColor: filledColor,
+        strokeColor: borderColor,
+        opacity: 0.9,
+        strokeWeight: 5,
+        fillOpacity: 1,
+        scale: 10
+      },
+      map: GLOBAL_MAP
+    });
+}
