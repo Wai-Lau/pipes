b4b0df5aab1b925e27a2d935af6ef15111f380b6
Wai-Lau <wl.wailau@gmail.com>
2019-02-13 20:06:20 -0500

    Fix DB issues

--- db/migrate/20181111020909_add_users_to_maproom.rb
+++
@@ -1,5 +0,0 @@
-class AddUsersToMaproom < ActiveRecord::Migration[5.0]
-  def change
-    add_column :maprooms, :user_id, :integer
-  end
-end
--- db/migrate/20181111022402_add_url_to_maproom.rb
+++ db/migrate/20181111022402_add_url_to_maproom.rb
@@ -1,5 +1,5 @@
 class AddUrlToMaproom < ActiveRecord::Migration[5.0]
   def change
-    add_column :maprooms, :url, :string
+    add_column :maprooms, :hsh, :string
   end
 end
